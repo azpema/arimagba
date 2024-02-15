5ffe868357c1062636839662a5df981614a2f3d9
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-02-15 23:16:25 +0100

    Add multiply and multiply accumulate operation

---
+++ gba-emulator/cpu/op/multiply_accumulate.cpp
@@ -0,0 +1,26 @@
+#include "multiply_accumulate.hpp"
+
+MultiplyAccumulate::MultiplyAccumulate(uint32_t op): OpCode::OpCode(op) {
+    a = Utils::getRegBits(op, A_FLAG_MASK, A_FLAG_SHIFT);
+    s = Utils::getRegBits(op, S_FLAG_MASK, S_FLAG_SHIFT);
+    rd = Utils::getRegBits(op, RD_FLAG_MASK, RD_FLAG_SHIFT);
+    rn = Utils::getRegBits(op, RN_FLAG_MASK, RN_FLAG_SHIFT);
+    rs = Utils::getRegBits(op, RS_FLAG_MASK, RS_FLAG_SHIFT);
+    rm = Utils::getRegBits(op, RM_FLAG_MASK, RM_FLAG_SHIFT);
+}   
+
+std::string MultiplyAccumulate::getSFlagMnemonic(){
+    return sFlag2Mnemonic[s];
+}
+
+std::string MultiplyAccumulate::getOpMnemonic(){
+    return op2Mnemonic[a];
+}
+
+std::string MultiplyAccumulate::toString(){
+    std::string mnemonic = getOpMnemonic() + getCondFieldMnemonic() + getSFlagMnemonic() + " " + \
+                           getRegMnemonic(rd) + " " + getRegMnemonic(rm) + " " + getRegMnemonic(rs);
+    if(a == 1)
+        mnemonic += " " + getRegMnemonic(rn);
+    return mnemonic;
+}
---
+++ gba-emulator/cpu/op/multiply_accumulate.hpp
@@ -0,0 +1,42 @@
+#ifndef _OPCODE_MULTIPLY_ACCUMULATE_ 
+#define _OPCODE_MULTIPLY_ACCUMULATE_ 
+
+#include <iostream>
+#include "opcode.hpp"
+#include "../../utils/utils.hpp"
+
+
+class MultiplyAccumulate : public OpCode {
+    private:
+        uint16_t a, s, rd, rn, rs, rm;
+        const static uint32_t A_FLAG_MASK = 0b00000000001000000000000000000000; 
+        const static uint32_t A_FLAG_SHIFT = 21; 
+
+        const static uint32_t S_FLAG_MASK = 0b00000000000100000000000000000000; 
+        const static uint32_t S_FLAG_SHIFT = 20;
+
+        const static uint32_t RD_FLAG_MASK = 0b00000000000011110000000000000000; 
+        const static uint32_t RD_FLAG_SHIFT = 16;
+
+        const static uint32_t RN_FLAG_MASK = 0b00000000000000001111000000000000; 
+        const static uint32_t RN_FLAG_SHIFT = 12;
+
+        const static uint32_t RS_FLAG_MASK = 0b00000000000000000000111100000000; 
+        const static uint32_t RS_FLAG_SHIFT = 8;
+
+        const static uint32_t RM_FLAG_MASK = 0b00000000000000000000000000001111; 
+        const static uint32_t RM_FLAG_SHIFT = 0;
+
+        std::string sFlag2Mnemonic[2] = {"", "s"};
+        std::string getSFlagMnemonic();
+
+        std::string op2Mnemonic[2] = {"mul", "mla"};
+        std::string getOpMnemonic();
+
+	public:
+		MultiplyAccumulate(uint32_t op);
+        std::string toString();
+
+};
+
+#endif
