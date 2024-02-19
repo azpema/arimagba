2a9af77ee12af3c8eacb595983aa5906fbad1be6
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-02-19 14:47:47 +0100

    Add single data swap op

---
+++ gba-emulator/cpu/op/single_data_swap.cpp
@@ -0,0 +1,20 @@
+#include "single_data_swap.hpp"
+
+SingleDataSwap::SingleDataSwap(uint32_t op): OpCode::OpCode(op) {
+    b = Utils::getRegBits(op, B_MASK, B_SHIFT);
+    rn = Utils::getRegBits(op, RN_MASK, RN_SHIFT);
+    rd = Utils::getRegBits(op, RD_MASK, RD_SHIFT);
+    rm = Utils::getRegBits(op, RM_MASK, RM_SHIFT);
+}   
+
+
+std::string SingleDataSwap::getBFlagMnemonic(){
+    return bFlag2Mnemonic[b];
+}
+
+
+
+std::string SingleDataSwap::toString(){
+    return "swp" + getCondFieldMnemonic() + getBFlagMnemonic() + " " + getRegMnemonic(rd) + "," + \
+            getRegMnemonic(rm) + ",[" + getRegMnemonic(rn) + "]";
+}
---
+++ gba-emulator/cpu/op/single_data_swap.hpp
@@ -0,0 +1,30 @@
+#ifndef _OPCODE_SINGLE_DATA_SWAP_
+#define _OPCODE_SINGLE_DATA_SWAP_
+
+#include <iostream>
+#include "opcode.hpp"
+
+class SingleDataSwap : public OpCode {
+    private:
+        uint16_t b, rn, rd, rm;
+
+        const static uint32_t B_MASK = 0b00000000010000000000000000000000;
+        const static uint32_t B_SHIFT = 22;
+        std::string bFlag2Mnemonic[2] = {"", "b"};
+        std::string getBFlagMnemonic(); 
+
+        const static uint32_t RN_MASK = 0b00000000000011110000000000000000; 
+        const static uint32_t RN_SHIFT = 16;
+
+        const static uint32_t RD_MASK = 0b00000000000000001111000000000000; 
+        const static uint32_t RD_SHIFT = 12;
+
+        const static uint32_t RM_MASK = 0b00000000000000000000000000001111; 
+        const static uint32_t RM_SHIFT = 0;
+
+	public:
+		SingleDataSwap(uint32_t op);
+        std::string toString();
+};
+
+#endif
