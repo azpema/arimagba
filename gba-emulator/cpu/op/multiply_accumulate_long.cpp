69ad4ff5f0517f9f652f3a1bb1de2a96cee92212
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-02-19 14:47:10 +0100

    Add multiply accumulate op

---
+++ gba-emulator/cpu/op/multiply_accumulate_long.cpp
@@ -0,0 +1,29 @@
+#include "multiply_accumulate_long.hpp"
+
+MultiplyAccumulateLong::MultiplyAccumulateLong(uint32_t op): OpCode::OpCode(op) {
+    u = Utils::getRegBits(op, U_FLAG_MASK, U_FLAG_SHIFT);
+    a = Utils::getRegBits(op, A_FLAG_MASK, A_FLAG_SHIFT);
+    s = Utils::getRegBits(op, S_FLAG_MASK, S_FLAG_SHIFT);
+    rdHi = Utils::getRegBits(op, RDHI_FLAG_MASK, RDHI_FLAG_SHIFT);
+    rdLo = Utils::getRegBits(op, RDLO_FLAG_MASK, RDLO_FLAG_SHIFT);
+    rs = Utils::getRegBits(op, RS_FLAG_MASK, RS_FLAG_SHIFT);
+    rm = Utils::getRegBits(op, RM_FLAG_MASK, RM_FLAG_SHIFT);
+}   
+
+std::string MultiplyAccumulateLong::getUFlagMnemonic(){
+    return uFlag2Mnemonic[u];
+}
+
+std::string MultiplyAccumulateLong::getSFlagMnemonic(){
+    return sFlag2Mnemonic[s];
+}
+
+std::string MultiplyAccumulateLong::getOpMnemonic(){
+    return op2Mnemonic[a];
+}
+
+std::string MultiplyAccumulateLong::toString(){
+    return getUFlagMnemonic() + getOpMnemonic() + getCondFieldMnemonic() + getSFlagMnemonic() + " " + \
+            getRegMnemonic(rdLo) + "," + getRegMnemonic(rdHi) + "," + getRegMnemonic(rm) + "," + \
+            getRegMnemonic(rs);
+}
---
+++ gba-emulator/cpu/op/multiply_accumulate_long.hpp
@@ -0,0 +1,46 @@
+#ifndef _OPCODE_MULTIPLY_ACCUMULATE_LONG_ 
+#define _OPCODE_MULTIPLY_ACCUMULATE_LONG_ 
+
+#include <iostream>
+#include "opcode.hpp"
+#include "../../utils/utils.hpp"
+
+
+class MultiplyAccumulateLong : public OpCode {
+    private:
+        uint16_t u, a, s, rdHi, rdLo, rs, rm;
+
+        const static uint32_t U_FLAG_MASK = 0b00000000010000000000000000000000;
+        const static uint32_t U_FLAG_SHIFT = 22;
+        std::string uFlag2Mnemonic[2] = {"u", "s"};
+        std::string getUFlagMnemonic();
+
+        const static uint32_t A_FLAG_MASK = 0b00000000001000000000000000000000; 
+        const static uint32_t A_FLAG_SHIFT = 21; 
+
+        const static uint32_t S_FLAG_MASK = 0b00000000000100000000000000000000; 
+        const static uint32_t S_FLAG_SHIFT = 20;
+        std::string sFlag2Mnemonic[2] = {"", "s"};
+        std::string getSFlagMnemonic();
+
+        const static uint32_t RDHI_FLAG_MASK = 0b00000000000011110000000000000000; 
+        const static uint32_t RDHI_FLAG_SHIFT = 16;
+
+        const static uint32_t RDLO_FLAG_MASK = 0b00000000000000001111000000000000; 
+        const static uint32_t RDLO_FLAG_SHIFT = 12;
+
+        const static uint32_t RS_FLAG_MASK = 0b00000000000000000000111100000000; 
+        const static uint32_t RS_FLAG_SHIFT = 8;
+
+        const static uint32_t RM_FLAG_MASK = 0b00000000000000000000000000001111; 
+        const static uint32_t RM_FLAG_SHIFT = 0;
+
+        std::string op2Mnemonic[2] = {"mull", "mlal"};
+        std::string getOpMnemonic();
+
+	public:
+		MultiplyAccumulateLong(uint32_t op);
+        std::string toString();
+};
+
+#endif
