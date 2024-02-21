4fbc747bf60a10394c293bc4d7c14866cd3495ed
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-02-21 21:54:39 +0100

    Add readHalfWord function

--- gba-emulator/bios.cpp
+++ gba-emulator/bios.cpp
@@ -20,15 +20,27 @@ uint32_t BIOS::readBytes(uint32_t offset, uint32_t nBytes){
     this->fileStream.read(reinterpret_cast<char*>(&word), nBytes);
     if (this->fileStream)
         return word;
-    else
+    else{
+        std::cerr << "ERROR: BIOS readBytes offset=" << offset << " nBytes=" << nBytes << std::endl;
+        return -1;
+    }
         return -1;
 }
 
 uint32_t BIOS::readWord(uint32_t offset){
     if (offset % 4 != 0){
-        std::cerr << "ERROR: Offset " << std::hex << offset << std::dec << " is not aligned to wordize" << std::endl;
+        std::cerr << "ERROR: Offset " << std::hex << offset << std::dec << " is not aligned to word size" << std::endl;
         return -1;
     }
 
     return readBytes(offset, 4);
+}
+
+uint16_t BIOS::readHalfWord(uint32_t offset){
+    if (offset % 2 != 0){
+        std::cerr << "ERROR: Offset " << std::hex << offset << std::dec << " is not aligned to half word size" << std::endl;
+        return -1;
+    }
+
+    return readBytes(offset, 2);
 }
--- gba-emulator/bios.hpp
+++ gba-emulator/bios.hpp
@@ -7,8 +7,10 @@ class BIOS {
     public:
         BIOS(std::string path);
         uint32_t readWord(uint32_t offset);
+        uint16_t readHalfWord(uint32_t offset);
     private:
         uint32_t readBytes(uint32_t offset, uint32_t nBytes);
+        
         std::string filePath;
         std::ifstream fileStream;
         
