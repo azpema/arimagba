69e0970a7e41d7a846d59f7f875b69790146ebb9
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-02-23 22:42:09 +0100

    Add thumb add/substract opcode

---
+++ gba-emulator/cpu/op/thumb/add_subtract.cpp
@@ -0,0 +1,26 @@
+#include "add_subtract.hpp"
+
+AddSubtract::AddSubtract(uint16_t op): ThumbOpCode::ThumbOpCode(op) {
+    i = Utils::getRegBits(op, I_MASK, I_SHIFT);
+    opField = Utils::getRegBits(op, OP_MASK, OP_SHIFT);
+    rnOffset3 = Utils::getRegBits(op, RN_OFFSET3_MASK, RN_OFFSET3_SHIFT);
+    rs = Utils::getRegBits(op, RS_MASK, RS_SHIFT);
+    rd = Utils::getRegBits(op, RD_MASK, RD_SHIFT);
+}   
+
+std::string AddSubtract::getOpMnemonic(){
+    return op2Mnemonic[opField];
+}
+
+std::string AddSubtract::toString(){
+    std::string mnemonic = getOpMnemonic() + " " + OpCode::getRegMnemonic(rd) + "," + OpCode::getRegMnemonic(rs) + ",";
+    if(i == 0){
+        mnemonic += OpCode::getRegMnemonic(rnOffset3);
+    }else if(i == 1){
+        mnemonic += "#" + Utils::toHexString(rnOffset3, 4);
+    }else{
+        std::cerr << "ERROR: Thumb Add/subtract invalid opcode field" << std::endl;
+    }
+
+    return mnemonic;
+}
---
+++ gba-emulator/cpu/op/thumb/add_subtract.hpp
@@ -0,0 +1,37 @@
+#ifndef _THUMB_OPCODE_ADD_SUBTRACT_ 
+#define _THUMB_OPCODE_ADD_SUBTRACT_ 
+
+#include <iostream>
+#include "thumb_opcode.hpp"
+#include "../opcode.hpp"
+#include "../../../utils/utils.hpp"
+
+class AddSubtract : public ThumbOpCode {
+    private:
+        uint16_t i, opField, rnOffset3, rs, rd;
+        
+        const static uint16_t I_MASK = 0b0000010000000000; 
+        const static uint16_t I_SHIFT = 10;
+
+        const static uint16_t OP_MASK = 0b0000001000000000;
+        const static uint16_t OP_SHIFT = 9;
+
+        const static uint16_t RN_OFFSET3_MASK = 0b0000000111000000;
+        const static uint16_t RN_OFFSET3_SHIFT = 6;
+
+        const static uint16_t RS_MASK = 0b0000000000111000;
+        const static uint16_t RS_SHIFT = 3;
+
+        const static uint16_t RD_MASK = 0b0000000000000111;
+        const static uint16_t RD_SHIFT = 0;
+
+        std::string op2Mnemonic[2] = {"add", "sub"};
+        std::string getOpMnemonic();
+
+	public:
+		AddSubtract(uint16_t op);
+        std::string toString();
+
+};
+
+#endif
