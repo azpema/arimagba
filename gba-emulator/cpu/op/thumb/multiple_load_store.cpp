b797a7bb5fba9ffa89fdcc37defe29dcb23ebe27
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-03-07 22:11:29 +0100

    Add multiple load/store thumb opcode

---
+++ gba-emulator/cpu/op/thumb/multiple_load_store.cpp
@@ -0,0 +1,29 @@
+#include "multiple_load_store.hpp"
+
+MultipleLoadStore::MultipleLoadStore(uint16_t op): ThumbOpCode::ThumbOpCode(op) {
+    l = Utils::getRegBits(op, L_MASK, L_SHIFT);
+    rb = Utils::getRegBits(op, RB_MASK, RB_SHIFT);
+    rList = Utils::getRegBits(op, RLIST_MASK, RLIST_SHIFT);
+    
+    for(size_t i = 0; i < 8; i++){
+        if((rList >> i) & 0x1 == 0x1)
+            registerListVec.push_back(i);    
+    }
+}   
+
+std::string MultipleLoadStore::getOpMnemonic(){
+    return op2Mnemonic[l];
+}
+
+std::string MultipleLoadStore::toString(){
+    std::string mnemonic = getOpMnemonic() + " " + OpCode::getRegMnemonic(rb) + "!,{";
+
+    for(auto it = registerListVec.begin(); it != registerListVec.end(); it++){
+        mnemonic += OpCode::getRegMnemonic(*it);
+        if(std::next(it) != registerListVec.end())
+            mnemonic += ",";
+    }
+    mnemonic += "}";
+    
+    return mnemonic;
+}
---
+++ gba-emulator/cpu/op/thumb/multiple_load_store.hpp
@@ -0,0 +1,32 @@
+#ifndef _THUMB_OPCODE_MULTIPLE_LOAD_STORE_ 
+#define _THUMB_OPCODE_MULTIPLE_LOAD_STORE_ 
+
+#include <iostream>
+#include "thumb_opcode.hpp"
+#include "../opcode.hpp"
+#include "../../../utils/utils.hpp"
+#include <vector>
+
+class MultipleLoadStore : public ThumbOpCode {
+    private:
+        uint8_t l, rb, rList;
+        std::vector<uint8_t> registerListVec;
+
+        const static uint16_t L_MASK = 0b0000100000000000; 
+        const static uint16_t L_SHIFT = 11;
+
+        const static uint16_t RB_MASK = 0b0000011100000000; 
+        const static uint16_t RB_SHIFT = 8;
+
+        const static uint16_t RLIST_MASK = 0b0000000011111111; 
+        const static uint16_t RLIST_SHIFT = 0;
+
+        std::string op2Mnemonic[2] = {"stmia", "ldmia"};
+        std::string getOpMnemonic();
+
+	public:
+		MultipleLoadStore(uint16_t op);
+        std::string toString();
+};
+
+#endif
