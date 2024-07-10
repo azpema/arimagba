4a9e199ba1a6320517da25cf7d89a8124186aee0
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-07-10 21:27:32 +0200

    Implement swap instruction

--- src/cpu/op/arm/single_data_swap.cpp
+++ src/cpu/op/arm/single_data_swap.cpp
@@ -21,8 +21,40 @@ void SingleDataSwap::doDecode(){
 
 }
 
+// TODO: Do not allow interruptions!
 void SingleDataSwap::doExecute(){
-    throw std::runtime_error("Error: Unimplemented instruction: SingleDataSwap");
+    if(rd == 15 || rn == 15 || rm == 15)
+        throw std::runtime_error("Error: SingleDataSwap PC register was used in Rd, Rn or Rs");
+
+    /*
+    
+    // Word
+            // LDR Force alignment
+            loadVal = cpu.getMemManager().readWord(baseRegVal & 0xFFFFFFFC);
+            if((baseRegVal & 0x3) != 0){
+                loadVal = Utils::rotateRight(loadVal, (baseRegVal & 0x3)*8);
+            }
+    
+    
+    */
+
+    uint8_t size = b ? 1 : 4;
+    uint32_t origSwapAddr = cpu.getReg(rn);
+    uint32_t alignedSwapAddr = origSwapAddr;
+    // Force align if word swap
+    if(b == 0){
+        alignedSwapAddr &= 0xFFFFFFFC;
+    }
+
+    uint32_t rmVal = cpu.getReg(rm);
+
+    uint32_t oldVal = cpu.getMemManager().read(alignedSwapAddr, size);
+    if(b == 0 && (origSwapAddr & 0x3) != 0){
+        oldVal = Utils::rotateRight(oldVal, (origSwapAddr & 0x3)*8);
+    }
+    cpu.getMemManager().store(alignedSwapAddr, rmVal, size);
+
+    cpu.setReg(rd, oldVal);
 }
 
 // SWP              1S+2N+1I
