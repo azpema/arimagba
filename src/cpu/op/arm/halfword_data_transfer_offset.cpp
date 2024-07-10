e55bc0f097341a1ef01b52978100e97aa5b99d7a
Markel Azpeitia Loiti <azpeitiamarkel@gmail.com>
2024-07-10 18:43:34 +0200

    Add initial halfword data transfer implementation (test 400-405 arm.gba)

--- src/cpu/op/arm/halfword_data_transfer_offset.cpp
+++ src/cpu/op/arm/halfword_data_transfer_offset.cpp
@@ -1,4 +1,5 @@
 #include "halfword_data_transfer_offset.hpp"
+#include <bitset>
 
 HalfwordDataTransferOffset::HalfwordDataTransferOffset(uint32_t op, ARM7TDMI &cpu): HalfwordDataTransfer::HalfwordDataTransfer(op, cpu) {
     offsetHi = Utils::getRegBits(op, OFFSETHI_FLAG_MASK, OFFSETHI_FLAG_SHIFT);
@@ -97,11 +98,30 @@ void HalfwordDataTransferOffset::doExecute(){
             }
         }else if(s == 1){
             if(h == 0){
-                // Signed byte
-                throw std::runtime_error("Error: Unimplemented HalfwordDataTransferOffset l=1 s=1 h=0");
+                /*
+                  Signed byte
+
+                  The LDRSB instruction loads the selected Byte into bits 7 to 0 of the destination
+                  register and bits 31 to 8 of the destination register are set to the value of bit 7, the sign
+                  bit
+                */ 
+                loadVal = cpu.getMemManager().readByte(baseRegVal);
+                uint8_t signBit = Utils::getRegSingleBit(loadVal, 7);
+
+                Utils::setRegBits(loadVal, 0xFFFFFF00, signBit ? 0xFFFFFF00 : 0);
+                
             }else if( h == 1){
-                // Signed halfwords
-                throw std::runtime_error("Error: Unimplemented HalfwordDataTransferOffset l=1 s=1 h=1");
+                /*
+                  Signed halfwords
+
+                  The LDRSH instruction loads the selected Half-word into bits 15 to 0 of the destination
+                  register and bits 31 to 16 of the destination register are set to the value of bit 15, the
+                  sign bit
+                */
+                loadVal = cpu.getMemManager().readHalfWord(baseRegVal);
+                uint8_t signBit = Utils::getRegSingleBit(loadVal, 15);
+
+                Utils::setRegBits(loadVal, 0xFFFF0000, signBit ? 0xFFFF0000 : 0);
             }
         }
     }
