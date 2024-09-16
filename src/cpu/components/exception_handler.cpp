2285c9a4830fe791ebcb3d356b0b56330f2a479a
Markel Azpeitia <azpeitiamarkel@gmail.com>
2024-09-16 19:54:04 +0200

    Convert exception enum to enum class

--- src/cpu/components/exception_handler.cpp
+++ src/cpu/components/exception_handler.cpp
@@ -19,7 +19,7 @@ void ExceptionHandler::raiseException(ARM7TDMI &cpu, Exception ex){
     cpu.getCPSR().setMode(newMode);
     cpu.getCPSR().setTFlag(false);
     cpu.getCPSR().setIFlag(true);
-    if(ex == RESET || ex == FIQ)
+    if(ex == Exception::RESET || ex == Exception::FIQ)
         cpu.getCPSR().setFFlag(true);
 
 
@@ -31,27 +31,29 @@ void ExceptionHandler::raiseException(ARM7TDMI &cpu, Exception ex){
     cpu.setMustFlushPipeline(true);
 
     switch(ex){
-        case RESET:
-        throw std::runtime_error("TODO: Unimplemented exception RESET in ExceptionHandler::raiseException");
-        break;
-        case UNDEF:
-        throw std::runtime_error("TODO: Unimplemented exception UNDEF in ExceptionHandler::raiseException");
-        break; 
-        case PREFETCH_ABORT:
-        throw std::runtime_error("TODO: Unimplemented exception PREFETCH_ABORT in ExceptionHandler::raiseException");
-        break; 
-        case DATA_ABORT:
-        throw std::runtime_error("TODO: Unimplemented exception DATA_ABORT in ExceptionHandler::raiseException");
-        break; 
-        case ADDRESS_EXCEED:
-        throw std::runtime_error("TODO: Unimplemented exception ADDRESS_EXCEED in ExceptionHandler::raiseException");
-        break; 
-        case FIQ:
-        throw std::runtime_error("TODO: Unimplemented exception FIQ in ExceptionHandler::raiseException");
-        break; 
-        case IRQ:
-        throw std::runtime_error("TODO: Unimplemented exception IRQ in ExceptionHandler::raiseException");
-        break; 
+        case Exception::RESET:
+            throw std::runtime_error("TODO: Unimplemented exception RESET in ExceptionHandler::raiseException");
+            break;
+        case Exception::UNDEF:
+            throw std::runtime_error("TODO: Unimplemented exception UNDEF in ExceptionHandler::raiseException");
+            break;
+        case Exception::PREFETCH_ABORT:
+            throw std::runtime_error("TODO: Unimplemented exception PREFETCH_ABORT in ExceptionHandler::raiseException");
+            break;
+        case Exception::DATA_ABORT:
+            throw std::runtime_error("TODO: Unimplemented exception DATA_ABORT in ExceptionHandler::raiseException");
+            break;
+        case Exception::ADDRESS_EXCEED:
+            throw std::runtime_error("TODO: Unimplemented exception ADDRESS_EXCEED in ExceptionHandler::raiseException");
+            break;
+        case Exception::FIQ:
+            throw std::runtime_error("TODO: Unimplemented exception FIQ in ExceptionHandler::raiseException");
+            break;
+        case Exception::IRQ:
+            throw std::runtime_error("TODO: Unimplemented exception IRQ in ExceptionHandler::raiseException");
+            break;
+        default:
+            break;
     }
 }
 
--- src/cpu/components/exception_handler.hpp
+++ src/cpu/components/exception_handler.hpp
@@ -11,31 +11,31 @@ class ARM7TDMI;
 class ExceptionHandler {
     public:
         ExceptionHandler();
-        enum Exception {RESET, UNDEF, SWI, PREFETCH_ABORT, DATA_ABORT, ADDRESS_EXCEED, FIQ, IRQ};
+        enum class Exception {RESET, UNDEF, SWI, PREFETCH_ABORT, DATA_ABORT, ADDRESS_EXCEED, FIQ, IRQ};
 
         void raiseException(ARM7TDMI &cpu, Exception ex);
 
     private:
         std::unordered_map<Exception, PSR::Mode> except2Mode = {
-            {RESET, PSR::Supervisor},
-            {UNDEF, PSR::Undefined},
-            {SWI, PSR::Supervisor},
-            {PREFETCH_ABORT, PSR::Abort},
-            {DATA_ABORT, PSR::Abort},
-            {ADDRESS_EXCEED, PSR::Supervisor},
-            {FIQ, PSR::IRQ},
-            {IRQ, PSR::FIQ},
+            {Exception::RESET,              PSR::Supervisor},
+            {Exception::UNDEF,              PSR::Undefined},
+            {Exception::SWI,                PSR::Supervisor},
+            {Exception::PREFETCH_ABORT,     PSR::Abort},
+            {Exception::DATA_ABORT,         PSR::Abort},
+            {Exception::ADDRESS_EXCEED,     PSR::Supervisor},
+            {Exception::FIQ,                PSR::IRQ},
+            {Exception::IRQ,                PSR::FIQ},
         };
 
         std::unordered_map<Exception, uint32_t> exceptionVector = {
-            {RESET,             0x00000000},
-            {UNDEF,             0x00000004},
-            {SWI,               0x00000008},
-            {PREFETCH_ABORT,    0x0000000C},
-            {DATA_ABORT,        0x00000010},
-            {ADDRESS_EXCEED,    0x00000014},
-            {FIQ,               0x00000018},
-            {IRQ,               0x0000001C},
+            {Exception::RESET,             0x00000000},
+            {Exception::UNDEF,             0x00000004},
+            {Exception::SWI,               0x00000008},
+            {Exception::PREFETCH_ABORT,    0x0000000C},
+            {Exception::DATA_ABORT,        0x00000010},
+            {Exception::ADDRESS_EXCEED,    0x00000014},
+            {Exception::FIQ,               0x00000018},    // GBA does not use this
+            {Exception::IRQ,               0x00000018},
         };
 };
 
